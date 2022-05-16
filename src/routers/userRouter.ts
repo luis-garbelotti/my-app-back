d4e1d2a516cb4f5a8e521acce4022f12e8192502
luis garbelotti <luisgsgarbelotti@gmail.com>
2022-05-16 20:33:16 -0300

    feat: sign in

--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -2,9 +2,11 @@ import { Router } from "express";
 import { validateSchemaMiddleware } from "../middlewares/validateSchemaMiddleware.js";
 import userController from "../controllers/userController.js";
 import userSchema from "../schemas/userSchema.js";
+import signInSchema from "../schemas/signInSchema.js";
 
 const userRouter = Router();
 
 userRouter.post('/signup', validateSchemaMiddleware(userSchema), userController.signUp)
+userRouter.post('/signin', validateSchemaMiddleware(signInSchema), userController.signIn)
 
 export default userRouter;
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,9 +1,17 @@
 import { User } from "@prisma/client";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
 import userRepository from "../repositories/userRepository.js";
-import { conflictError, } from "../utils/errorUtils.js";
-import { hashData } from "../utils/hashDataUtils.js";
+import {
+  conflictError,
+  notFoundError,
+  unauthorizedError,
+} from "../utils/errorUtils.js";
+import { compareHashData, hashData } from "../utils/hashDataUtils.js";
+dotenv.config(); 
 
 export type CreateUserData = Omit<User, "id">;
+export type LoginUserData = Omit<User, "id" | "name">; 
 
 async function signUp(createUserData: CreateUserData) {
   const existingUser = await userRepository.findByEmail(createUserData.email);
@@ -14,6 +22,25 @@ async function signUp(createUserData: CreateUserData) {
   await userRepository.insert({ ...createUserData, password: hashedPassword });
 }
 
+async function signIn(loginData: LoginUserData) {
+  const user = await getUserOrFail(loginData);
+
+  const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET);
+
+  return token;
+}
+
+async function getUserOrFail(loginData: LoginUserData) {
+  const user = await userRepository.findByEmail(loginData.email);
+  if (!user) throw unauthorizedError("Invalid credentials");
+
+  const isPasswordValid = compareHashData(loginData.password, user.password)
+  if (!isPasswordValid) throw unauthorizedError("Invalid credentials");
+
+  return user;
+} 
+
 export default {
   signUp,
+  signIn
 }
