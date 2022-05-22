30da5cc73fb989e6f4030023fba53cadfe90cb58
luis garbelotti <luisgsgarbelotti@gmail.com>
2022-05-22 15:03:03 -0300

    cd: auto deploy on heroku

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
-import cors from 'cors';
 import express, { json } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import { errorHandlerMiddleware } from './middlewares/errorHandlerMiddleware.js';
 import router from './routers/index.js';
 
 const app = express();
-app.use(json());
 app.use(cors());
+app.use(json());
 app.use(router);
 app.use(errorHandlerMiddleware);
 
