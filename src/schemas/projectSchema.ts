40371c2e633c4746603dc9fe18092dc2d132d643
luis garbelotti <luisgsgarbelotti@gmail.com>
2022-05-23 13:26:20 -0300

    feat: change project schema

--- src/schemas/projectSchema.ts
+++ src/schemas/projectSchema.ts
@@ -1,12 +1,13 @@
 import Joi from 'joi';
 
 const projectSchema = Joi.object({
-  title: Joi.string().required(),
+  title: Joi.string().trim().required(),
   resume: Joi.string().required(),
   importantInfos: Joi.string().required(),
   startDate: Joi.date().required(),
   limitDate: Joi.date().required(),
   clientId: Joi.number().required(),
+  value: Joi.number().required(),
 });
 
 export default projectSchema;
